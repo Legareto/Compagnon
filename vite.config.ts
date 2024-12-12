02510075ad5d7fe2cb26a99b3a643d6d3325b1fc
Tommy Légaré <tommy.legare@uqat.ca>
2024-12-12 18:51:07 -0500

    fix: config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Compagnon/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Compagnon/" : "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
-});
+}));

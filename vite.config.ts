d32356b7696724af700a4729708f9ed48546e0b6
Tommy Légaré <tommy.legare@uqat.ca>
2024-12-12 23:28:27 -0500

    fix: deploy and running build locally

--- vite.config.ts
+++ vite.config.ts
@@ -8,21 +8,4 @@ export default defineConfig(({ mode }) => ({
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-      },
-      output: {
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-  },
-  define: {
-    "process.env.BASE_URL": JSON.stringify(
-      mode === "production" ? "/Compagnon/" : "/"
-    ),
-  },
 }));
